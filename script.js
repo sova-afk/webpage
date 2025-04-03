d5c5224eec82ff3bdfce8b51ef84b1de1643c687
sova-afk <kajko.kucera1@gmail.com>
2025-04-03 09:18:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 <script>
-  // Check if the current URL path is within the /assets/images/ directory
   if (window.location.pathname.startsWith('/assets/images/')) {
-    window.location.href = '/';  // Redirect to homepage or other page
+    window.location.href = '/';
   }
 </script>
